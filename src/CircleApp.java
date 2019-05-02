101d55d92325f2e83ddba61573449fbc45da88e0
LinmeiJ <linmeijiang@hotmail.com>
2019-05-02 16:40:37 -0400

    second commit

--- src/CircleApp.java
+++ src/CircleApp.java
@@ -5,17 +5,18 @@ public class CircleApp {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the Circle Tester");
 		String userInputString = "y";
-	
+		int circNum = 0;  //initializing number of circles user have build
 		while(userInputString.equalsIgnoreCase("y")) {
 
 			double radius = Validator.getDouble(sc, "Enter Radius: ");
 			Circle cc = new Circle(radius);
 			System.out.println("Circumference: " + cc.getFormattedCircumference());
-			System.out.println("Area: " + cc.getFormattedCircumference());
-			
+			System.out.println("Area: " + cc.getFormattedArea());
+			circNum++;
 			System.out.println("Continue?(y/n): ");
 			userInputString = sc.nextLine();
+		
 		}
-		System.out.println("Goodbye!");
+		System.out.println("You have build " + circNum + " circles in this program.Goodbye!");
 	}
 }
